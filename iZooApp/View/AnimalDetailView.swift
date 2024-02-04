52eed6f27363914bb21533d377ed1bb07bb67cb5
Elfana, A | Elfaael <elfaael12@gmail.com>
2024-02-04 10:53:21 +0700

    feat: integrate MapKit

--- iZooApp/View/AnimalDetailView.swift
+++ iZooApp/View/AnimalDetailView.swift
@@ -14,9 +14,64 @@ struct AnimalDetailView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(alignment: .center, spacing: 20) {
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
                 
-                Text(animal.name)
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
+                
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+                    InsetGalleryView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+                Group {
+                    HeadingView(headingImage: "questionmark.circle", headingText: "Did you know?")
+                    InsetFactView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+                Group {
+                    HeadingView(headingImage: "info.circle", headingText: "All about \(animal.name)")
+                    Text(animal.description)
+                        .multilineTextAlignment(.leading)
+                        .layoutPriority(1)
+                }
+                .padding(.horizontal)
+                
+                // MAP
+                Group {
+                    HeadingView(headingImage: "map", headingText: "National Parks")
+                    InsetMapView()
+                }
+                .padding(.horizontal)
+                
+                
+                // Link
+                Group {
+                    HeadingView(headingImage: "map", headingText: "National Parks")
+                }
+                .padding(.horizontal)
+
             }
+           
             .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
         }
     }
@@ -28,7 +83,7 @@ struct AnimalDetailView_Previews: PreviewProvider {
 
     static var previews: some View {
         NavigationView {
-            AnimalDetailView(animal: animals[2])
+            AnimalDetailView(animal: animals[0])
         }
        
     }
