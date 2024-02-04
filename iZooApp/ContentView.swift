a63ce72a06c9604dcb719065369c327774a44410
Elfana, A | Elfaael <elfaael12@gmail.com>
2024-02-04 08:37:11 +0700

    Initial Commit

---
+++ iZooApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iZooApp
+//
+//  Created by Elfana Anamta Chatya on 04/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iZooApp/iZooAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iZooAppApp.swift
+//  iZooApp
+//
+//  Created by Elfana Anamta Chatya on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iZooAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
